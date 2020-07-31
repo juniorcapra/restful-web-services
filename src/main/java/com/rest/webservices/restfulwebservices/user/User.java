d027bd105c8bf7ad3567c50e110b482414bc5d92
juniorcapra <acaprajr@gmail.com>
2020-07-31 19:47:19 -0300

    Create a Post users

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -3,23 +3,23 @@ package com.rest.webservices.restfulwebservices.user;
 import java.util.Date;
 import javax.persistence.Entity;
 
-@ApiModel(description="All details about the user.")
-@Entity
+//@ApiModel(description="All details about the user.")
+//@Entity
 public class User {
-    @Id
-    @GeneratedValue
+//    @Id
+//    @GeneratedValue
     private Integer id;
 
-    @Size(min=2, message="Name should have atleast 2 characters")
-    @ApiModelProperty(notes="Name should have atleast 2 characters")
+//    @Size(min=2, message="Name should have atleast 2 characters")
+//    @ApiModelProperty(notes="Name should have atleast 2 characters")
     private String name;
 
-    @Past
-    @ApiModelProperty(notes="Birth date should be in the past")
+//    @Past
+//    @ApiModelProperty(notes="Birth date should be in the past")
     private Date birthDate;
 
-    @OneToMany(mappedBy="user")
-    private List<Post> posts;
+//    @OneToMany(mappedBy="user")
+//    private List<Post> posts;
 
     protected User() {
 
@@ -56,13 +56,13 @@ public class User {
         this.birthDate = birthDate;
     }
 
-    public List<Post> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(List<Post> posts) {
-        this.posts = posts;
-    }
+//    public List<Post> getPosts() {
+//        return posts;
+//    }
+//
+//    public void setPosts(List<Post> posts) {
+//        this.posts = posts;
+//    }
 
     @Override
     public String toString() {
