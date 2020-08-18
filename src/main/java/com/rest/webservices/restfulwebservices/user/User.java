56ba2e2b239138eb97358d085c99c24baeb31b7f
juniorcapra <acaprajr@gmail.com>
2020-08-18 20:46:06 -0300

    Create Post Entity

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -3,9 +3,11 @@ package com.rest.webservices.restfulwebservices.user;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -25,8 +27,16 @@ public class User {
     @ApiModelProperty(notes="Birth date should be in the past")
     private Date birthDate;
 
-//    @OneToMany(mappedBy="user")
-//    private List<Post> posts;
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @OneToMany(mappedBy="user")
+    private List<Post> posts;
 
     protected User() {
 
